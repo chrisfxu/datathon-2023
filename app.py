3d516140ab269b0abcc4361cb8253130709cf31c
Bryant Park <bryantpark04@gmail.com>
2023-04-30 16:14:54 -0400

    Smooth out curve

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import streamlit as st
 from models.rf_regressor_model import RandomForestRegressorModel
+from scipy.interpolate import make_interp_spline
 
 
 st.set_page_config(layout='wide')
@@ -26,7 +29,24 @@ model = get_model()
 
 with results:
     '### Will your lungs be fine?'
-    st.write(model.predict(age, gender == 'Man', smoking_status == 'Yes', weeks_since_scan, lung_capacity, percent_capacity)[0])
+    ages_projected = np.arange(age + 5, 123, 1)
+    weeks_since_scan_projected = (ages_projected - age) * 52 + weeks_since_scan
+    Y = []
+    for i in range(len(ages_projected)):
+        Y.append(model.predict(ages_projected[i], gender == 'Man', smoking_status == 'Yes', weeks_since_scan_projected[i], lung_capacity, percent_capacity)[0])
+    #define x as 200 equally spaced values between the min and max of original x 
+    xnew = np.linspace(ages_projected.min(), ages_projected.max(), 200) 
+
+    #define spline with degree k=7
+    spl = make_interp_spline(ages_projected, Y, k=9)
+    y_smooth = spl(xnew)
+
+    #create smooth line chart 
+    plt.plot(xnew, y_smooth)
+    # plt.plot(ages_projected, Y)
+    plt.xlabel('Age (years)')
+    plt.ylabel('Projected Lung Capacity (mL)')
+    st.pyplot(plt)
 
 with surf:
     #st.markdown(r"![Doctors Hate This One Simple Trick!](https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.RkJFdQILTedyGsDtxLQq6gAAAA%26pid%3DApi&f=1&ipt=443df8bcabc60c25c19a73158b7031be13a4008845ef4ee2d4973afe9197c6c6&ipo=images)")
--- models/rf_regressor_model.py
+++ models/rf_regressor_model.py
@@ -16,13 +16,11 @@ class RandomForestRegressorModel(BaseModel):
             self.model = pickle.load(f)
 
     def train_and_store_model(self) -> RandomForestRegressor:
-
         train = pd.read_csv('data/train.csv')
         train['FVCLag'] = train.FVC.shift(1)
         train['PercentLag'] = train.Percent.shift(1)
 
         uq = set()
-
         for index, row in train.iterrows():
             if row['Patient'] not in uq:
                 uq.add(row['Patient'])
@@ -40,6 +38,7 @@ class RandomForestRegressorModel(BaseModel):
             pickle.dump(regressor, f)
     
     def predict(_self, age: int, is_male: bool, does_smoke: bool, weeks_since_scan: int, capacity_lag: float, percent_lag: float):
+        # Weeks  Age  Sex  SmokingStatus  FVCLag  PercentLag
         return _self.model.predict([[weeks_since_scan, age, is_male, does_smoke, capacity_lag, percent_lag]])
 
 
